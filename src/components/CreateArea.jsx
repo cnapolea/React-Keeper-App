dceef32a561c62121c9c01db75e5eef5db4c4c2e
Cjnapoleao.Dev <cjnapoleao.dev@Cristianos-MacBook-Pro.local>
2020-11-29 14:50:20 -0600

    use react material-ui for app styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,9 +1,14 @@
 import React, {useState} from "react";
 
+import { Grow } from '@material-ui/core';
+import { Zoom } from '@material-ui/core';
+import { Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 
 function CreateArea(props) {
 
   const [note, setNote] = useState({title:"", content:""});
+  const [shouldExpand, setExpand] = useState(false);
 
   function handleNoteChange(e){
     const {name, value} = e.target;
@@ -17,10 +22,13 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" onChange={handleNoteChange}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleNoteChange}/>
-        <button type="button" onClick={() => props.submit(note)}>Add</button>
+      <form className="create-note">
+        <input name="title" placeholder="Title" onChange={handleNoteChange} hidden={shouldExpand?false:true}/>
+        <textarea name="content" placeholder="Take a note..." rows={shouldExpand?"3":"1"} onChange={handleNoteChange} onClick={ () => setExpand(prev => !prev) }/>
+
+        <Zoom in={shouldExpand?true:false}>
+          <Fab onClick={() => props.submit(note)}><AddIcon /></Fab> 
+        </Zoom>
       </form>
     </div>
   );
